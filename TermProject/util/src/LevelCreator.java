a9c2e37e7b65ab3fb20fc831d59a79061618e200
Ümit Kaan Usta <53405015+umitkaanusta@users.noreply.github.com>
2020-05-04 14:31:56 +0300

    checked LevelCreator

--- TermProject/util/src/LevelCreator.java
+++ TermProject/util/src/LevelCreator.java
@@ -7,127 +7,110 @@ import java.util.Scanner;
 public class LevelCreator {
 
    public static Tile[][] createLevel(int level) throws FileNotFoundException {
-
-       Tile[][] grid = new Tile[4][4];
-        ArrayList<Tile> tiles = readInput(level);
-
-        int pos = 0;
-        for (int row = 0; row < 4; row++){
-            for (int col = 0; col < 4; col++){
-
-                grid[col][row] = tiles.get(pos);
-                pos++;
-
-            }
-        }
-
-        return grid;
+	   Tile[][] board = new Tile[4][4];
+       ArrayList<Tile> tiles = readInput(level);
+       int pos = 0;
+       for (int i = 0; i < 4; i++) {
+           for (int j = 0; j < 4; j++) {
+               board[i][j] = tiles.get(pos);
+               pos++;
+           }
+       }
+       return board;
     }
 
     private static ArrayList<Tile> readInput(int level) throws FileNotFoundException {
-
-       ArrayList<Tile> tiles = new ArrayList<>();
-
-       File file = new File("levels/level" + level + ".txt");
-       Scanner sc = new Scanner(file);
-
-               while(sc.hasNext()){
-
-                   String line = sc.nextLine();
-                   String[] lineinput = line.split(",");
-                   int id = Integer.parseInt(lineinput[0]);
-                   int x = getxCord(id);
-                   int y = getyCord(id);
-                   String type = lineinput[1];
-                   String property = lineinput[2];
-
-                   switch (type){
-
-                       case "Starter":
-                           if(property.equalsIgnoreCase("Vertical"))
-                               tiles.add(new PipeStatic(id,x,y,true,false,true,false));
-
-                           else
-                               tiles.add(new PipeStatic(id,x,y,true,false,false,true));
-
-                           break;
-
-                       case "Empty":
-
-                            if(property.equalsIgnoreCase("none"))
-                                tiles.add(new Empty(id,x,y,false));
-
-                            else
-                                tiles.add(new Free(id,x,y));
-
-                       break;
-
-                       case "Pipe":
-                           if (property.equalsIgnoreCase("Vertical"))
-                               tiles.add(new Pipe(id,x,y,false,true));
-
-                           else if(property.equalsIgnoreCase("Horizontal"))
-                               tiles.add(new Pipe(id,x,y,false,false));
-
-                           else if(property.equalsIgnoreCase("00"))
-                               tiles.add(new Curved(id,x,y,false,0,0));
-
-                           else if(property.equalsIgnoreCase("01"))
-                               tiles.add(new Curved(id,x,y,false,0,1));
-
-                           else if(property.equalsIgnoreCase("10"))
-                               tiles.add(new Curved(id,x,y,false,1,0));
-
-                           else if(property.equalsIgnoreCase("11"))
-                               tiles.add(new Curved(id,x,y,false,1,1));
-
-                           break;
-
-                       case "End":
-                           if (property.equalsIgnoreCase("Horizontal"))
-                               tiles.add(new PipeStatic(id,x,y,false,true,false,true));
-
-                           else
-                               tiles.add(new PipeStatic(id,x,y,false,true,true,false));
-
-                           break;
-
-                       case "PipeStatic":
-                           if (property.equalsIgnoreCase("Horizontal"))
-                               tiles.add(new PipeStatic(id,x,y,false,false,false,true));
-
-                           else if (property.equalsIgnoreCase("Vercital"))
-                               tiles.add(new PipeStatic(id,x,y,false,true,true,false));
-
-                           else if(property.equalsIgnoreCase("01"))
-                               tiles.add((new PipeStatic(id,x,y,false,false,true,true)));
-
-                           break;
-
-                   }
-               }
-    return tiles;
-   }
-
-    public int getMaxLevel(){
-       File file = new File("levels/");
-       return Objects.requireNonNull(file.listFiles()).length;
-
+    	// Reads text files and fills the ArrayList tiles
+    	try {
+    		ArrayList<Tile> tiles = new ArrayList<>();
+        	File file = new File("levels/" + level + ".txt");
+        	Scanner sc = new Scanner(file);
+        	
+        	while(sc.hasNext()) {
+        		String line = sc.nextLine();
+        		if(line.trim().equalsIgnoreCase("")) {
+        			// If line is empty, just continue
+        			continue;
+        		}
+        		createTileFromInput(line, tiles);
+        	}
+    	} catch(IOException e) {
+    		System.out.println("There is a problem with the input file. Try again.");
+    	}
+    	return tiles;
+    }
+    
+    private static void createTileFromInput(String line, baseList ArrayList<Tile>) {
+		String[] input = line.split(","); // Putting id, type, property in an array
+		int id = Integer.parseInt(input[0]);
+		String type = input[1];
+		String property = input[2];
+		int x = getxCord(id);
+		int y = getyCord(id);
+		
+		switch(type) {
+		case "Starter":
+			if property.equalsIgnoreCase("Vertical") {
+				baseList.add(new PipeStatic(id, x, y, true, false, false, null));
+			}
+			else {
+				baseList.add(new PipeStatic(id, x, y, true, false, null, false));
+			}
+			break;
+		case "End":
+			if property.equalsIgnoreCase("Vertical") {
+				baseList.add(new PipeStatic(id, x, y, false, true, false, null));
+			}
+			else {
+				baseList.add(new PipeStatic(id, x, y, false, true, null, false));
+			}
+			break;
+		case "PipeStatic":
+			if property.equalsIgnoreCase("Vertical") {
+				baseList.add(new Pipe(id, x, y, true, true));
+			}
+			else if property.equalsIgnoreCase("Horizontal") {
+				baseList.add(new Pipe(id, x, y, true, false);
+			}
+			else {
+				baseList.add(new PipeStatic(id, x, y, false, false, true, true));
+			}
+			break;
+		case "Pipe":
+			if property.equalsIgnoreCase("Vertical") {
+				baseList.add(new Pipe(id, x, y, false, true));
+			}
+			else if property.equalsIgnoreCase("Horizontal") {
+				baseList.add(new Pipe(id, x, y, false, false));
+			}
+			else {
+				int position1 = Integer.parseInt(property.charAt(0) + "");
+				int position2 = Integer.parseInt(property.charAt(1) + "");
+				baseList.add(new Curved(id, x, y, position1, position2));
+			}
+			break;
+		case "Empty":
+			if property.equalsIgnoreCase("Free") {
+				baseList.add(new Empty(id, x, y, true));
+			}
+			else {
+				baseList.add(new Free(id, x, y));
+			}
+			break;
+		}
     }
 
-    private static int getxCord(int position){
-       int x = 0;
-       x = (position - 1) % 4;
-
-       return x;
+    public static int getMaxLevel() {
+    	File file = new File("levels/");
+    	return Objects.requireNonNull(file.listFiles()).length;
     }
 
-    private static int getyCord(int position){
-       int y = 0;
-       y = (position - 1) / 4;
-
-       return y;
+    private static int getxCord(int position) {
+    	return (position - 1) % 4;
     }
 
-
+    private static int getyCord(int position) {
+    	return (position - 1) / 4;
+    }
+    
 }
